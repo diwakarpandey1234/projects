5622fbb58a90b863d29394da8c54d910e96dd556
diwakarpandey1234 <diwakarrp1@gmail.com>
2026-08-25 17:10:17 +0530

    Delete src/components/Input.jsx

--- src/components/Input.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Input({placeholder,text,onChange}){
-    return(
-        <input className='bg-gray-800' placeholder={placeholder} value={text} onChange={onChange} />
-    )
-}
-
-export f
